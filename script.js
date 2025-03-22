c2817e2b92cc90d813994ba8e502ea9dcd16dde3
AnaisPH <anaisphconsul@gmail.com>
2025-03-22 14:33:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Manejo del formulario de cita
+
     document.getElementById('appointmentForm').addEventListener('submit', function (event) {
         event.preventDefault();
 
@@ -13,14 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = whatsappUrl;
     });
 
-    // Detectar el scroll y aplicar estilo al elemento activo
     window.addEventListener('scroll', function () {
         var scrollPosition = window.scrollY;
 
-        // Obtener todos los enlaces de la barra de navegación
         var navLinks = document.querySelectorAll('header nav ul li a');
 
-        // Iterar sobre los enlaces para verificar la posición de cada sección
         navLinks.forEach(function (link) {
             var sectionId = link.getAttribute('href').substring(1);
             var section = document.getElementById(sectionId);
@@ -36,14 +33,11 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Obtén el modal
     var modal = document.getElementById("myModal");
 
-    // Obtén la imagen y la inserta dentro del modal
     var img = document.querySelectorAll('.services-gallery img');
     var modalImg = document.getElementById("img01");
 
-    // Agrega el evento de clic a cada imagen
     img.forEach(function (image) {
         image.onclick = function () {
             modal.style.display = "block";
@@ -51,15 +45,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Obtén el elemento <span> que cierra el modal
     var span = document.getElementsByClassName("close")[0];
 
-    // Cuando el usuario hace clic en <span> (x), cierra el modal
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-    // Cuando el usuario hace clic fuera de la imagen, cierra el modal
     modal.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
